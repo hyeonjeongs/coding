b4ecbb631137ac33be96ccc1d4531359fc377778
hyeonjeong Park <py0429@ewhain.net>
2022-06-12 22:41:55 +0900

    user repo

--- instagramclone/src/main/java/com/technicalinterview/instagramclone/Repository/UserRepo.java
+++ instagramclone/src/main/java/com/technicalinterview/instagramclone/Repository/UserRepo.java
@@ -4,10 +4,13 @@ import com.technicalinterview.instagramclone.Entity.Users;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface UserRepo extends CrudRepository<Users, Integer> { //<entity, primary key의 타입>
     Users save(Users user);
+
     Users findByUserId(String userId);
 
-
+    ArrayList<Users> findAll();
 }
